1c1a88db20be9099574946ded829f3a8c7338639
PrathikshaKini <kiniprathiksha18@gmail.com>
2023-02-10 23:11:45 +0530

    added order-interaction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { OrderinteractionComponent } from './components/orderinteraction/orderinteraction.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SideBarComponent],
+  declarations: [AppComponent, HeaderComponent, SideBarComponent, OrderinteractionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/orderinteraction/orderinteraction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orderinteraction',
+  templateUrl: './orderinteraction.component.html',
+  styleUrls: ['./orderinteraction.component.css']
+})
+export class OrderinteractionComponent {
+  
+}
