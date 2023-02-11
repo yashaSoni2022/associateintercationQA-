7794989b3538cb830a0f75d9c05fc70995391948
PrathikshaKini <kiniprathiksha18@gmail.com>
2023-02-11 21:42:13 +0530

    completed ui of screens

--- src/app/components/orderinteraction/orderinteraction.component.ts
+++ src/app/components/orderinteraction/orderinteraction.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./orderinteraction.component.css']
 })
 export class OrderinteractionComponent {
-  
+  Condition1: boolean = true;
+  autosave: boolean = true;
+  bottomSymbol:boolean=true;
+  delete1=true;
+  delete2=true;
+  delete3=true;
+  Condition2: boolean = false;
+  Condition3: boolean = false;
+
+  screen2(){
+    this.Condition1=false;
+    this.autosave=false;
+    this.delete1=false;
+    this.delete2=false;
+    this.delete3=false;
+    this.bottomSymbol=false;
+    this.Condition2=true;
+  }
+
+  screen3(){
+    this.Condition2=false;
+    this.Condition3=true;
+  }
+
+  deleteicon1(){
+    this.delete1=false;
+  }
+
+  deleteicon2(){
+    this.delete2=false;
+  }
+
+  deleteicon3(){
+    this.delete3=false;
+  }
+
+  dismiss(){
+    this.bottomSymbol=false;
+  }
+
+  dismissbox(){
+    this.Condition1=false;
+    this.autosave=false;
+  }
 }
